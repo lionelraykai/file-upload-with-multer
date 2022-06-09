77508698a8f8c74612d136ec9eae2ebcec2eeb0f
SHUBHAM KUMAR <you@example.com>
2022-06-09 15:02:04 +0530

    fourth commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const multer = require('multer')
+const app = express()
+
+const fileStorageEngine = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'./images')
+    },
+    filename:(req,file,cb)=>{
+        cb(null, Date.now() + '--' + file.originalname)
+    }
+})
+
+const upload = multer({storage: fileStorageEngine})
+
+app.post('/single',upload.single('image'),(req,res)=>{
+    console.log(req.file)
+    res.send('file upload')
+})
+
+app.post('/multiple',upload.array('images',3),(req,res)=>{
+    console.log(req.files)
+    res.send('multiple files upload')
+})
+
+app.listen(6000)
